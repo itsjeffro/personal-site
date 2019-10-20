af0f81c008ce41d3fe9e49957ed37125dc4ae1d8
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-10-20 17:05:07 +1100

    Added settings views

---
+++ resources/views/settings/partials/submenu.blade.php
@@ -0,0 +1,3 @@
+<div class="list-group">
+    <a href="{{ route('settings.profile.show') }}" class="list-group-item list-group-item-action">Profile</a>
+</div>
---
+++ resources/views/settings/profile-show.blade.php
@@ -0,0 +1,36 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-3">
+            @include('settings.partials.submenu')
+        </div>
+        <div class="col-md-9">
+            <h2>Public profile</h2>
+
+            <form method="POST">
+                <input type="hidden" name="_method" value="PUT">
+                @csrf
+
+                <div class="form-group">
+                    <label class="label-control" for="name">Name</label>
+                    <input type="text" class="form-control" id="name" value="{{ $user->name }}">
+                </div>
+
+                <div class="form-group">
+                    <label class="label-control" for="email">Email</label>
+                    <input type="text" class="form-control" id="email" value="{{ $user->email }}">
+                </div>
+
+                <div class="form-group">
+                    <label class="label-control" for="role">Role</label>
+                    <input type="text" readonly class="form-control-plaintext" id="role" value="--">
+                </div>
+
+                <button class="btn btn-primary" type="submit" name="submit">Update profile</button>
+            </form>
+        </div>
+    </div>
+</div>
+@endsection
