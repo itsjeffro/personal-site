9b16bba1b55a1bd512aec8f7b73005516a123641
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-03 17:59:48 +1100

    Added users menu item

--- resources/views/backend/layouts/app.blade.php
+++ resources/views/backend/layouts/app.blade.php
@@ -36,6 +36,9 @@
                         <li class="nav-item">
                             <a class="nav-link" href="{{ route('home') }}">Home</a>
                         </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('users.list') }}">Users</a>
+                        </li>
                     </ul>
 
                     <!-- Right Side Of Navbar -->
