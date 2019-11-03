7710c3d82eef400031ada6a8b95b04fd8852989f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-03 15:53:10 +1100

    Removed client secret from table

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,11 +5,11 @@
     <div class="row justify-content-center">
         <div class="col-md-12">
             @if (session('status'))
-                <div classs="form-group">
-                    <div class="alert alert-success" role="alert">
-                        {{ session('status') }}
-                    </div>
+            <div classs="form-group">
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
                 </div>
+            </div>
             @endif
 
             <div class="form-group">
@@ -20,35 +20,38 @@
                 <div class="card-header">OAuth</div>
 
                 @if($clients->count() === 0)
-                    <div class="card-body text-center">
-                        <a href="{{ route('oauth.create') }}" class="btn btn-link">Get started by adding a client</a>
-                    </div>
+                <div class="card-body text-center">
+                    <a href="{{ route('oauth.create') }}" class="btn btn-link">Get started by adding a client</a>
+                </div>
                 @else
-                    <div class="table-responsive">
-                        <table class="table">
-                            <thead>
-                                <tr>
-                                    <th>Name</th>
-                                    <th>Client ID</th>
-                                    <th>Client Secret</th>
-                                    <th>Type</th>
-                                </tr>    
-                            </thead>
+                <div class="table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th width="30%">Name</th>
+                                <th width="20%">Client ID</th>
+                                <th>Type</th>
+                                <th></th>
+                            </tr>
+                            </tr>
+                        </thead>
 
-                            <tbody>
-                                @foreach($clients as $client)
-                                    <tr>
-                                        <td>
-                                            <a href="{{ route('oauth.show', ['oauth_client' => $client->id]) }}">{{$client->name}}</a>
-                                        </td>
-                                        <td>{{$client->id}}</td>
-                                        <td>{{$client->secret}}</td>
-                                        <td>{{$client->grant_type_name}}</td>
-                                    </tr>
-                                @endforeach  
-                            </tbody>
-                        </table>
-                    </div>
+                        <tbody>
+                            @foreach($clients as $client)
+                            <tr>
+                                <td width="30%">
+                                    <a href="{{ route('oauth.show', ['oauth_client' => $client->id]) }}">{{$client->name}}</a>
+                                </td>
+                                <td width="20%">{{$client->id}}</td>
+                                <td>{{$client->grant_type_name}}</td>
+                                <td class="text-right">
+                                    <a href="{{ route('oauth.show', ['oauth_client' => $client->id]) }}" title="View">View</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
                 @endif
             </div>
         </div>
