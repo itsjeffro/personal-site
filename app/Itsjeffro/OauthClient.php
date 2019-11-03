5390866764713695d0c1c26e51dc8b1144114e79
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-03 15:52:00 +1100

    Moved constants to OauthTypes class

--- app/Itsjeffro/OauthClient.php
+++ app/Itsjeffro/OauthClient.php
@@ -6,18 +6,6 @@ use Laravel\Passport\Client;
 
 class OauthClient extends Client
 {
-    /** @var integer */
-    const PASSWORD_GRANT_TYPE = 1;
-
-    /** @var string */
-    const PASSWORD_GRANT_NAME = 'Password';
-
-    /** @var integer */
-    const PERSONAL_ACCESS_GRANT_TYPE = 2;
-
-    /** @var string */
-    const PERSONAL_ACCESS_GRANT_NAME = 'Personal access';
-
     /**
      * Returns grant type name.
      *
@@ -26,13 +14,39 @@ class OauthClient extends Client
     public function getGrantTypeNameAttribute(): string
     {
         if ($this->personal_access_client) {
-            return self::PERSONAL_ACCESS_GRANT_NAME;
+            return OauthTypes::PERSONAL_ACCESS_GRANT_NAME;
         }
 
         if ($this->password_client) {
-            return self::PASSWORD_GRANT_NAME;
+            return OauthTypes::PASSWORD_GRANT_NAME;
+        }
+
+        if (empty($this->password_client) && empty($this->personal_access_client)) {
+            return OauthTypes::CLIENT_CREDENTIALS_GRANT_NAME;
         }
 
         return '';
+    }
+
+    /**
+     * Returns grant type id.
+     *
+     * @return int
+     */
+    public function getGrantTypeIdAttribute(): int
+    {
+        if ($this->personal_access_client) {
+            return OauthTypes::PERSONAL_ACCESS_GRANT_TYPE;
+        }
+
+        if ($this->password_client) {
+            return OauthTypes::PASSWORD_GRANT_TYPE;
+        }
+
+        if (empty($this->password_client) && empty($this->personal_access_client)) {
+            return OauthTypes::CLIENT_CREDENTIALS_GRANT_TYPE;
+        }
+
+        return 0;
     }
 }
