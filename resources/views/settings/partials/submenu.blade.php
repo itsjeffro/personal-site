bcd7b5e2257e7483a14a4fde77a11dd27b6aef91
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-03 15:54:08 +1100

    Added sub menus for account and security

--- resources/views/settings/partials/submenu.blade.php
+++ resources/views/settings/partials/submenu.blade.php
@@ -1,3 +1,5 @@
 <div class="list-group">
     <a href="{{ route('settings.profile.show') }}" class="list-group-item list-group-item-action">Profile</a>
+    <a href="{{ route('settings.profile.show') }}" class="list-group-item list-group-item-action">Account</a>
+    <a href="{{ route('settings.profile.show') }}" class="list-group-item list-group-item-action">Security</a>
 </div>
