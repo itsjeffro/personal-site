047a79880da9e89a9e3d128f68cff7203b6875c6
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-10 09:59:56 +1100

    Added permissions seed data

--- database/seeds/RolesTableSeeder.php
+++ database/seeds/RolesTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Permission;
 use Spatie\Permission\Models\Role;
 
 class RolesTableSeeder extends Seeder
@@ -12,6 +13,19 @@ class RolesTableSeeder extends Seeder
      */
     public function run()
     {
+        Permission::insert([
+            ['name' => 'users:get', 'guard_name' => 'web'],
+            ['name' => 'users:list', 'guard_name' => 'web'],
+            ['name' => 'users:create', 'guard_name' => 'web'],
+            ['name' => 'users:update', 'guard_name' => 'web'],
+            ['name' => 'users:delete', 'guard_name' => 'web'],
+            ['name' => 'roles:get', 'guard_name' => 'web'],
+            ['name' => 'roles:list', 'guard_name' => 'web'],
+            ['name' => 'roles:create', 'guard_name' => 'web'],
+            ['name' => 'roles:update', 'guard_name' => 'web'],
+            ['name' => 'roles:delete', 'guard_name' => 'web'],
+        ]);
+
         Role::create(['name' => 'admin']);
         Role::create(['name' => 'user']);
     }
