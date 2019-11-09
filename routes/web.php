8e2bdb360d01949114e220cde3c8a83c51f5126e
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-09 20:30:06 +1100

    Added account and sercurity routes

--- routes/web.php
+++ routes/web.php
@@ -1,15 +1,7 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
 
 Auth::routes();
 
@@ -17,16 +9,22 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/settings/profile', 'Backend\SettingsProfileController@show')->name('settings.profile.show');
+    Route::get('/settings/profile', 'Backend\SettingsProfileController@show')->name('settings.profile.show');
+    Route::get('/settings/account', 'Backend\SettingsAccountController@show')->name('settings.account.show');
+    Route::get('/settings/security', 'Backend\SettingsSecurityController@show')->name('settings.security.show');
+    Route::put('/settings/security', 'Backend\SettingsSecurityController@update')->name('settings.security.update');
 
-Route::get('/applications/create', 'Backend\OauthController@create')->name('oauth.create');
-Route::post('/applications', 'Backend\OauthController@store')->name('oauth.store');
-Route::get('/applications/{oauth_client}', 'Backend\OauthController@show')->name('oauth.show');
-Route::put('/applications/{oauth_client}', 'Backend\OauthController@update')->name('oauth.update');
-Route::delete('/applications/{oauth_client}', 'Backend\OauthController@destroy')->name('oauth.delete');
+    Route::get('/applications/create', 'Backend\OauthController@create')->name('oauth.create');
+    Route::post('/applications', 'Backend\OauthController@store')->name('oauth.store');
+    Route::get('/applications/{oauth_client}', 'Backend\OauthController@show')->name('oauth.show');
+    Route::put('/applications/{oauth_client}', 'Backend\OauthController@update')->name('oauth.update');
+    Route::delete('/applications/{oauth_client}', 'Backend\OauthController@destroy')->name('oauth.delete');
+});
 
-Route::get('/users', 'Backend\UsersController@index')->name('users.list');
-
-Route::get('/roles', 'Backend\RolesController@index')->name('roles.list');
+Route::group(['middleware' => 'role:admin'], function () {
+    Route::get('/users', 'Backend\UsersController@index')->name('users.list');
+    Route::get('/roles', 'Backend\RolesController@index')->name('roles.list');
+});
