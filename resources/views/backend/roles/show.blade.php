64cae3b7d740980bf5ebdafad2f24776ea75006b
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-10 20:41:35 +1100

    Added form and save button

--- resources/views/backend/roles/show.blade.php
+++ resources/views/backend/roles/show.blade.php
@@ -10,31 +10,38 @@
                {{ $role->name }}
             </div>
 
-            <div class="card">
-                <div class="card-header">
-                    Permissions
+            <form method="POST" action="{{ route('roles.update', ['role' => $role->id]) }}">
+                <input type="hidden" name="_method" value="PUT">
+                @csrf
+
+                <div class="card mb-3">
+                    <div class="card-header">
+                        Permissions
+                    </div>
+                    <div class="table-responsive">
+                        <table class="table">
+                            <thead>
+                                <th></th>
+                                <th>Name</th>
+                            </thead>
+                            <tbody>
+                                @foreach($permissions as $permission)
+                                    <tr>
+                                        <td width="5%">
+                                            <input id="{{ $permission->id }}" type="checkbox" name="permission[{{ $permission->id }}]">
+                                        </td>
+                                        <td>
+                                            {{ $permission->name }}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <th></th>
-                            <th>Name</th>
-                        </thead>
-                        <tbody>
-                            @foreach($permissions as $permission)
-                                <tr>
-                                    <td width="5%">
-                                        <input id="{{ $permission->id }}" type="checkbox" name="permission[{{ $permission->id }}]">
-                                    </td>
-                                    <td>
-                                        {{ $permission->name }}
-                                    </td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
+
+                <button class="btn btn-primary" type="submit" name="update">Update role</button>
+            </form>
         </div>
     </div>
 </div>
