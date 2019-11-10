bcef6adc80eacc99fcdfa26851f8654d4742e1af
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-11-10 17:27:47 +1100

    Added initial users create, show create form, update methods

--- app/Http/Controllers/Backend/UsersController.php
+++ app/Http/Controllers/Backend/UsersController.php
@@ -4,21 +4,34 @@ namespace App\Http\Controllers\Backend;
 
 use App\Http\Controllers\Controller;
 use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\RedirectResponse;
 
 class UsersController extends Controller
 {
     /** @var User */
     private $user;
 
+    /** @var Validator */ 
+    private $validator;
+
+    /** @var Hash */
+    private $hash;
+
     /**
-     * Create a new controller instance.
+     * UsersController.
      *
-     * @return void
+     * @param User $user
+     * @param Validator $validator
+     * @param Hash $hash
      */
-    public function __construct(User $user)
+    public function __construct(User $user, Validator $validator, Hash $hash)
     {
-        $this->middleware('auth');
         $this->user = $user;
+        $this->validator = $validator;
+        $this->hash = $hash;
     }
 
     /**
@@ -31,5 +44,71 @@ class UsersController extends Controller
         return view('backend.users.list')->with([
             'users' => $this->user->paginate(),
         ]);
+    }
+    
+    /**
+     * Show create form.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function create()
+    {
+        return view('backend.users.create');
+    }
+
+    /**
+     * Create record.
+     *
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function store(Request $request)
+    {
+        $validator = $this->validator::make($request->all(), [
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|confirmed|min:8',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()
+                ->route('users.create')
+                ->withErrors($validator)
+                ->withInput();
+        }
+
+        $this->user->create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => $this->hash::make($request->input('password')),
+        ]);
+
+        return redirect()->route('users.list');
+    }
+    
+    /**
+     * Show record.
+     *
+     * @param User $user
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show(User $user)
+    {
+        return view('backend.users.show')->with([
+            'user' => $user,
+            'roles' => $user->getRoleNames()->implode(', '),
+        ]);
+    }
+
+    /**
+     * Update record.
+     *
+     * @param Request $request
+     * @param User $user
+     * @return RedirectResponse
+     */
+    public function update(Request $request, User $user)
+    {
+        return redirect()->route('backend.users.show');
     }
 }
