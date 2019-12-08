d974f7ec2d66338bed8ed150f6611d6366c14c2d
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-08 19:38:41 +1100

    Restructured tables

--- resources/views/backend/roles/list.blade.php
+++ resources/views/backend/roles/list.blade.php
@@ -2,53 +2,42 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-3">
-            @include('backend.partials.user-role-submenu')
-        </div>
-
-        <div class="col-md-9">
-            <div class="form-group">
-                <a href="" class="btn btn-primary btn-sm">Create role</a>
-            </div>
-
-            <div class="card">
-                <div class="card-header">Roles</div>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th width="20%">Role name</th>
-                                <th width="40%">Users</th>
-                                <th width="40%">Created at</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach($roles as $role)
-                            <tr>
-                                <td width="20%">
-                                    <a href="{{ route('roles.show', ['role' => $role->id]) }}" title="View role">{{ $role->name }}</a>
-                                </td>
-                                <td width="40%">
-                                    @if(isset($userCountPerRole[$role->id]))
-                                        {{ $userCountPerRole[$role->id]->total_users }}
-                                    @else
-                                        0
-                                    @endif
-                                </td>
-                                <td width="40%">
-                                {{ $role->created_at }}
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                
-                {{ $roles->links() }}
-            </div>
-        </div>
+  <div class="row justify-content-center">
+    <div class="col-md-3">
+      @include('backend.partials.user-role-submenu')
     </div>
+
+    <div class="col-md-9">
+      <div class="heading">
+        <a href="" class="btn btn-primary btn-sm float-right">Create role</a>
+        <h2>Roles</h2>
+      </div>
+
+      <div class="table-responsive">
+        <table class="table table-blocks">
+          <tbody>
+            @foreach($roles as $role)
+            <tr>
+              <td width="20%">
+                <a href="{{ route('roles.show', ['role' => $role->id]) }}" title="View role">{{ $role->name }}</a>
+              </td>
+              <td width="40%">
+                @if(isset($userCountPerRole[$role->id]))
+                  Users: {{ $userCountPerRole[$role->id]->total_users }}
+                @else
+                  Users: 0
+                @endif
+              </td>
+              <td width="40%">
+                Created: {{ $role->created_at }}
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      
+      {{ $roles->links() }}
+  </div>
 </div>
 @endsection
--- resources/views/backend/users/list.blade.php
+++ resources/views/backend/users/list.blade.php
@@ -2,45 +2,38 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-3">
-            @include('backend.partials.user-role-submenu')
-        </div>
-
-        <div class="col-md-9">
-            <div class="form-group">
-                <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm">Add user</a>
-            </div>
-
-            <div class="card">
-                <div class="card-header">Users</div>
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th width="33%">Email</th>
-                                <th>Roles</th>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach($users as $user)
-                            <tr>
-                                <td width="33%">
-                                    <a href="{{ route('users.show', ['user' => $user->id]) }}" title="View {{ $user->email }}">{{ $user->email }}</a>
-                                </td>
-                                <td>
-                                    {{ $user->getRoleNames()->implode(', ') }}
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                
-                {{ $users->links() }}
-            </div>
-        </div>
+  <div class="row justify-content-center">
+    <div class="col-md-3">
+        @include('backend.partials.user-role-submenu')
     </div>
+
+    <div class="col-md-9">
+      <div class="heading">
+        <a href="{{ route('users.create') }}" class="btn btn-primary btn-sm float-right">Add user</a>
+        <h2>Users</h2>
+      </div>
+
+      <div class="table-responsive">
+        <table class="table table-blocks">
+          <tbody>
+            @foreach($users as $user)
+            <tr>
+              <td width="33%">
+                  <a href="{{ route('users.show', ['user' => $user->id]) }}" title="View {{ $user->email }}">{{ $user->email }}</a>
+              </td>
+              <td>
+                  Role: {{ $user->getRoleNames()->implode(', ') }}
+              </td>
+              <td>
+                  Created: {{ $user->created_at->format('Y-m-d H:i:s') }}
+              </td>
+            </tr>
+            @endforeach
+          </tbody>
+        </table>
+      </div>
+      
+      {{ $users->links() }}
+  </div>
 </div>
 @endsection
