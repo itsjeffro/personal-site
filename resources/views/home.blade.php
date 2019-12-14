f905987ce4320ed3ece12f2ab87d8051b093e940
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-15 10:11:34 +1100

    Moved application to own page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,47 +12,8 @@
             </div>
             @endif
 
-            <div class="form-group">
-                <a href="{{ route('oauth.create') }}" class="btn btn-primary btn-sm">New OAuth App</a>
-            </div>
-
-            <div class="card">
-                <div class="card-header">OAuth</div>
-
-                @if($clients->count() === 0)
-                <div class="card-body text-center">
-                    <a href="{{ route('oauth.create') }}" class="btn btn-link">Get started by adding a client</a>
-                </div>
-                @else
-                <div class="table-responsive">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th width="30%">Name</th>
-                                <th width="20%">Client ID</th>
-                                <th>Type</th>
-                                <th></th>
-                            </tr>
-                            </tr>
-                        </thead>
-
-                        <tbody>
-                            @foreach($clients as $client)
-                            <tr>
-                                <td width="30%">
-                                    <a href="{{ route('oauth.show', ['oauth_client' => $client->id]) }}">{{$client->name}}</a>
-                                </td>
-                                <td width="20%">{{$client->id}}</td>
-                                <td>{{$client->grant_type_name}}</td>
-                                <td class="text-right">
-                                    <a href="{{ route('oauth.show', ['oauth_client' => $client->id]) }}" title="View">View</a>
-                                </td>
-                            </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-                @endif
+            <div class="heading">
+                <h2>Dashboard</h2>
             </div>
         </div>
     </div>
