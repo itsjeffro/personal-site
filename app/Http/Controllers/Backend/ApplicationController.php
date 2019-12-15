a1ab60f88133c1ab7ae21dad6baa861dab463bf2
Jeff Nielsen <jefftoddnielsen@gmail.com>
2019-12-15 18:28:47 +1100

    Inject user model into construct

--- app/Http/Controllers/Backend/ApplicationController.php
+++ app/Http/Controllers/Backend/ApplicationController.php
@@ -5,17 +5,21 @@ namespace App\Http\Controllers\Backend;
 use App\Http\Controllers\Controller;
 use App\Itsjeffro\OauthClient;
 use Illuminate\Support\Facades\Auth;
+use App\User;
 
 class ApplicationController extends Controller
 {
+    /** @var User */
+    private $user;
+
     /**
      * Create a new controller instance.
      *
      * @return void
      */
-    public function __construct()
+    public function __construct(User $user)
     {
-        $this->middleware('auth');
+        $this->user = $user;
     }
 
     /**
@@ -25,7 +29,7 @@ class ApplicationController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
+        $user = $this->user::find(Auth::id());
         $clients = OauthClient::where(['user_id' => $user->id]);
         
         if ($user->hasRole('admin')) {
