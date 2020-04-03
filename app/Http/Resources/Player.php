25d1c979d2552c906b6f4b3717b94c115e7a3bdc
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-03 19:56:22 +1100

    Added api resources for player and stats

---
+++ app/Http/Resources/Player.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+use App\Http\Resources\PlayerStats as PlayerStatsResource;
+
+class Player extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'player_name' => $this->name,
+            'is_admin' => $this->admin_count > 0,
+            'stats' => new PlayerStatsResource($this->whenLoaded('playerStats')),
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
+        ];
+    }
+}
