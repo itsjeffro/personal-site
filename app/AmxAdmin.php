3d76432b0fbedf3d1559eb9d5b70f8b14cdf7846
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-04 13:51:43 +1100

    Added relatioships

--- app/AmxAdmin.php
+++ app/AmxAdmin.php
@@ -9,6 +9,35 @@ class AmxAdmin extends Model
     /** @var string */
     protected $table = 'admins';
 
+    /** @var string|null */
+    protected $primaryKey = null;
+
+    /** @var bool */
+    public $incrementing = false;
+
+    /** @var bool */
+    public $timestamps = false;
+
     /** @var int */
     const DEFAULT_PER_PAGE = 25;
+
+    /**
+     * Admin's player record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function player()
+    {
+        return $this->hasOne(Player::class, 'steam_id', 'auth');
+    }
+
+    /**
+     * Admin's player stats record.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function playerStats()
+    {
+        return $this->hasOne(PlayerStats::class, 'steam_id', 'auth');
+    }
 }
