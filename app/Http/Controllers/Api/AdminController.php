573ac499a44d953b18e6b7c8ebf3640b645fbab1
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-04 13:53:58 +1100

    Added show and update methods

--- app/Http/Controllers/Api/AdminController.php
+++ app/Http/Controllers/Api/AdminController.php
@@ -2,39 +2,82 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Player;
+use App\AmxAdmin as Admin;
 use Illuminate\Http\Request;
-use App\Http\Resources\Player as PlayerResource;
+use App\Http\Resources\Admin as AdminResource;
 
 class AdminController
 {
-    /** @var Player */
-    private $player;
+    /** @var Admin */
+    private $admin;
 
     /**
-     * PlayerController
+     * AdminController
      *
-     * @param Player $player
+     * @param Admin $admin
      */
-    public function __construct(Player $player)
+    public function __construct(Admin $admin)
     {
-        $this->player = $player;
+        $this->admin = $admin;
     }
 
     /**
-     * List players.
+     * List admins.
      *
      * @return \Illuminate\Http\JsonResponse
      */
     public function index(Request $request)
     {
-        $perPage = (int) $request->get('per_page', Player::DEFAULT_PER_PAGE);
+        $perPage = (int) $request->get('per_page', Admin::DEFAULT_PER_PAGE);
 
-        $players = $this->player
-            ->withCount(['admin'])
-            ->has('admin')
+        $admins = $this->admin
+            ->with(['player'])
             ->paginate($perPage);
 
-        return PlayerResource::collection($players);
+        return AdminResource::collection($admins);
+    }
+
+    /**
+     * Get admin.
+     *
+     * @param string $auth
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function show(string $auth)
+    {
+        $admin = $this->admin
+            ->with(['player', 'playerStats'])
+            ->where('auth', $auth)
+            ->first();
+
+        if (!$admin instanceof Admin) {
+            return response()->json(['message' => sprintf('Admin could not be found with auth [%s]', $auth)], 404);
+        }
+
+        return new AdminResource($admin);
+    }
+
+    /**
+     * Update admin.
+     *
+     * @param Request $request
+     * @param string $auth
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function update(Request $request, string $auth)
+    {
+        $admin = $this->admin
+            ->where('auth', $auth)
+            ->first();
+
+        if (!$admin instanceof Admin) {
+            return response()->json(['message' => sprintf('Admin could not be found with auth [%s]', $auth)], 404);
+        }
+
+        $admin->password = "test";
+
+        $admin->save();
+
+        return new AdminResource($admin, 200);
     }
 }
