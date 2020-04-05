c5d1cc2ac07b2e44d74174ddaa56b41766394b9f
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-05 16:56:00 +1000

    Fixed up as utc original

--- resources/js/pages/Home/components/PlayersTable.js
+++ resources/js/pages/Home/components/PlayersTable.js
@@ -20,35 +20,45 @@ export const PlayersTable = (props) => {
           </tr>
         </thead>
         <tbody>
-          {players.map(player => (
-            <tr key={player.id}>
-              <td>
-                {player.id}
-              </td>
-              <td>
-                <img 
-                  className="rounded" 
-                  src={player.avatar ? player.avatar : '/images/player_default.jpg'} 
-                  width="25" 
-                  height="25"
-                  alt={player.player_name} 
-                  title={player.player_name + ' avatar'} 
-                />
-              </td>
-              <td>
-                {player.player_name}
-              </td>
-              <td>
-                {player.stats ? player.stats.kills : 0}
-              </td>
-              <td>
-                {player.stats ? player.stats.deaths : 0}
-              </td>
-              <td className="text-right">
-                {player.updated_at ? moment(player.updated_at, 'YYYY-MM-DD hh:ss').format('D, MMM YYYY - hh:ss A') : 'N/A'}
-              </td>
-            </tr>
-          ))}
+          {players.map(player => {
+            let updatedAt = 'N/A';
+            let updatedAtRaw = 'N/A';
+
+            if (player.updated_at) {
+              updatedAt = moment(player.updated_at, 'YYYY-MM-DD hh:mm:ss').utc().format('DD, MMM YYYY - hh:mm A');
+              updatedAtRaw = player.updated_at;
+            }
+
+            return (
+              <tr key={player.id}>
+                <td>
+                  {player.id}
+                </td>
+                <td>
+                  <img 
+                    className="rounded" 
+                    src={player.avatar ? player.avatar : '/images/player_default.jpg'} 
+                    width="25" 
+                    height="25"
+                    alt={player.player_name} 
+                    title={player.player_name + ' avatar'} 
+                  />
+                </td>
+                <td>
+                  {player.player_name}
+                </td>
+                <td>
+                  {player.stats ? player.stats.kills : 0}
+                </td>
+                <td>
+                  {player.stats ? player.stats.deaths : 0}
+                </td>
+                <td className="text-right">
+                  <span title={updatedAtRaw}>{ updatedAt }</span>
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </div>
