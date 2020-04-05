8b15e6b6f3d251494ac034ef1f8dbe4e6c3b9d55
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-05 16:30:34 +1000

    Added navbar component

---
+++ resources/js/components/Navbar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const Navbar = (props) => {
+  return (
+    <nav className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">Itsjeffro.com</a>
+    </nav>
+  )
+}
