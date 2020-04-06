5936e2066b9ca483d929d4e4f90021db4022fbdc
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-06 13:47:17 +1000

    Added player stats controller

---
+++ app/Http/Controllers/Api/External/PlayerStatsController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Http\Controllers\Api\External;
+
+use App\PlayerStats;
+use Illuminate\Http\Request;
+use App\Http\Resources\PlayerStats as PlayerStatsResource;
+use App\Game\Sort;
+
+class PlayerStatsController
+{
+    /** @var PlayerStats */
+    private $playerStats;
+
+    /**
+     * PlayerController
+     *
+     * @param PlayerStats $playerStats
+     */
+    public function __construct(PlayerStats $playerStats)
+    {
+        $this->playerStats = $playerStats;
+    }
+
+    /**
+     * List player stats.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index(Request $request)
+    {
+        $perPage = (int) $request->get('per_page', PlayerStats::DEFAULT_PER_PAGE);
+
+        // Sorts
+        $sortableColumns = [
+            'id' => 'id', 
+            'kills' => 'kills',
+            'deaths' => 'deaths',
+            'name' => 'name',
+        ];
+
+        $defaultSort = 'id:asc';
+        
+        $requestedSort = $request->get('sort', $defaultSort);
+
+        $sort = new Sort($requestedSort, $defaultSort, $sortableColumns);
+
+        $playerStats = $this->playerStats
+            ->with('player')
+            ->orderBy($sort->getColumn(), $sort->getOrder())
+            ->paginate($perPage);
+
+        return PlayerStatsResource::collection($playerStats);
+    }
+}
