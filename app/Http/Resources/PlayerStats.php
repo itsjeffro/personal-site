1d3b02a98fd06ca842c1256b8441de6b9317974a
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-06 13:47:34 +1000

    Updated model and resource

--- app/Http/Resources/PlayerStats.php
+++ app/Http/Resources/PlayerStats.php
@@ -15,8 +15,16 @@ class PlayerStats extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
+            'player_name' => $this->name,
+            'avatar' => $this->player->avatar,
             'kills' => $this->kills,
             'deaths' => $this->deaths,
+            'hits' => $this->hits,
+            'shots' => $this->shots,
+            'headshots' => $this->headshots,
+            'created_at' => $this->created_at,
+            'updated_at' => $this->updated_at,
         ];
     }
 }
--- app/PlayerStats.php
+++ app/PlayerStats.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class PlayerStats extends Model
 {
     use SoftDeletes;
+
+    /** @var int */
+    const DEFAULT_PER_PAGE = 25;
+
+    /**
+     * Player's overall stats.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasOne
+     */
+    public function player()
+    {
+        return $this->hasOne(Player::class, 'steam_id', 'steam_id');
+    }
 }
