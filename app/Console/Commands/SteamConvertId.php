ea5866e1b78b5be5f99b7d89a8abac1518472bdd
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-07 08:44:16 +1000

    Updated command to insert new record instead of updating

--- app/Console/Commands/SteamConvertId.php
+++ app/Console/Commands/SteamConvertId.php
@@ -3,8 +3,11 @@
 namespace App\Console\Commands;
 
 use App\Player;
+use App\PlayerStats;
+use DateTime;
 use InvalidArgumentException;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
 use SteamID;
 use Psr\Log\LoggerInterface;
 
@@ -24,22 +27,30 @@ class SteamConvertId extends Command
      */
     protected $description = 'Detects the currently stored Steam ID and converts it to Steam ID 64';
 
-    /**
-     * @var LoggerInterface
-     */
+    /** @var LoggerInterface */
     private $log;
+
+    /** @var Player */
+    private $player;
+
+    /** @var PlayerStats */
+    private $playerStats;
 
     /**
      * Create a new command instance.
      *
      * @param LoggerInterface $log
+     * @param Player $player
+     * @param PlayerStats $playerStats
      * @return void
      */
-    public function __construct(LoggerInterface $log)
+    public function __construct(LoggerInterface $log, Player $player, PlayerStats $playerStats)
     {
         parent::__construct();
 
         $this->log = $log;
+        $this->player = $player;
+        $this->playerStats = $playerStats;
     }
 
     /**
@@ -47,24 +58,32 @@ class SteamConvertId extends Command
      *
      * @return mixed
      */
-    public function handle(Player $player)
+    public function handle()
     {
-        $players = $player->whereNull('steam_id_64')->get();
+        $playerStats = $this->playerStats
+            ->select('player_stats.steam_id')
+            ->leftJoin('players', 'players.steam_id', '=', 'player_stats.steam_id')
+            ->where('player_stats.steam_id', '!=', '')
+            ->whereNull('players.steam_id')
+            ->get();
 
-        if (count($players) < 1) {
+        if (count($playerStats) < 1) {
             $this->info(sprintf('[%s] There were no players that required updating.', $this->signature));
 
             return;
         }
 
-        foreach ($players as $player) {
+        $players = [];
+
+        foreach ($playerStats as $stats) {
             $steamId64 = 0;
 
             try {
-                $steam = new SteamID($player->steam_id);
+                $steam = new SteamID($stats->steam_id);
+
                 $steamId64 = $steam->ConvertToUInt64();
             } catch (InvalidArgumentException $e) {
-                $message = sprintf('Player ID [%d] could not convert Steam ID [%s]', $player->id, $player->steam_id);
+                $message = sprintf('Player stats ID [%d] could not convert Steam ID [%s]', $stats->id, $stats->steam_id);
 
                 $this->info($message);
 
@@ -72,11 +91,17 @@ class SteamConvertId extends Command
                     'message' => $e->getMessage(),
                 ]);
             } finally {
-                $player->steam_id_64 = $steamId64;
-                $player->save();
-
-                $this->info(sprintf('Player ID [%d] created Steam ID 64 [%s] from Steam ID [%s]', $player->id, $steamId64, $player->steam_id));
+                $players[] = [
+                    'steam_id' => $stats->steam_id,
+                    'steam_id_64' => $steamId64,
+                    'created_at' => new DateTime(),
+                    'updated_at' => new DateTime(),
+                ];
             }
+        }
+
+        if (count($players) > 0) {
+            Player::insert($players);
         }
 
         $this->info(sprintf('[%s] Finished', $this->signature));
