efe330ef79de2f505e09d05bb40b92d35ebe0dcd
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-26 09:56:03 +1000

    Added custom claims

--- app/User.php
+++ app/User.php
@@ -68,6 +68,9 @@ class User extends Authenticatable implements JWTSubject
      */
     public function getJWTCustomClaims()
     {
-        return [];
+        return [
+            'name' => $this->name,
+            'picture' => '',
+        ];
     }
 }
