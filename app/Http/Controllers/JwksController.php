8c73b49fd027ff17d92b905a57f9316da1f9e4e5
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-25 19:01:34 +1000

    Return list of public keys in jwks

--- app/Http/Controllers/JwksController.php
+++ app/Http/Controllers/JwksController.php
@@ -11,16 +11,24 @@ class JwksController
      */
     public function index(): JsonResponse
     {
-        $keys = [
+        $fileContents = file_get_contents(config('jwt.keys.public'));
+        $fileContents = str_replace('-----BEGIN CERTIFICATE-----', '', $fileContents);
+        $fileContents = str_replace('-----END CERTIFICATE-----', '', $fileContents);
+
+        $key = [
             'alg' => 'RSA256',
             'kty' => 'RSA',
             'use' => 'sig',
             'kid' => 'jwt_id_rsa',
-            'x5t' => config('jwt.keys.public'),
+            'x5c' => [
+                preg_replace("/\r|\n/", '', $fileContents)
+            ],
         ];
 
         return response()->json([
-            'keys' => $keys,
+            'keys' => [
+                $key
+            ]
         ]);
     }
 }
