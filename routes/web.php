36bc49b494cc83a67ed00dbbd8bcb2deca7e256e
Jeff Nielsen <jefftoddnielsen@gmail.com>
2020-04-27 20:48:30 +1000

    Changed rout path so it does not conflict with the react route

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,6 @@ Route::get('/.well-known/jwks.json', 'JwksController@index');
 
 Route::get('/steam/login', 'SteamAuthController@login');
 
-Route::get('/steam/auth', 'SteamAuthController@authenticate');
+Route::get('/steam/authenticate', 'SteamAuthController@authenticate');
 
 Route::get('/{view?}', 'HomeController@index')->where('view', '(.*)');
